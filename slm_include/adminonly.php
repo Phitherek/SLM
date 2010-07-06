7ee845651de918b66d86a8eafc8eeefea231eea9
Phitherek <phitherek@gmail.com>
2010-07-06 16:28:16 +0200

    small changes

--- slm_include/adminonly.php
+++ slm_include/adminonly.php
@@ -1,12 +1,15 @@
 <?php
-function slm_adminonly() {
+function slm_adminonly($link="index.php",$footerlinkpage="index.php", $footerlinktext="Indeks") {
 	session_start();
 	if($_SESSION['slm_type'] != "admin") {
 		?>
-		<p class="slm_error">Ta strona dostępna jest tylko dla użytkownika SLM ze statusem administratora! Jeżeli powinieneś mieć do niej dostęp, skontaktuj się z administratorem.</p><br /><br />
+		<p class="slm_error">Ta strona dostępna jest tylko dla użytkownika SLM ze statusem administratora! Jeżeli powinieneś mieć do niej dostęp, skontaktuj się z administratorem.<br /><br /><a class="slm_link" href="<?php echo $link; ?>" alt="link">Przejdź do strony głównej</a></p><br /><br />
 		<?php
-		include("footer.php");
-		slm_footer();
+		?>
+<hr />
+<p class="slm_footer">Powered by SLM | &copy; 2010 by Phitherek_<br /><br />
+<a class="slm_link" href="<?php echo $footerlinkpage; ?>" alt="link"><?php echo $footerlinktext; ?></a></p>
+<?php
 		die();
 	}
 }
