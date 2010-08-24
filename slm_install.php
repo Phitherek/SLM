ce6d0587f177e46aec046c98d1df175d70b0e7d9
Phitherek <phitherek@gmail.com>
2010-08-24 20:16:17 +0200

    translated slm_install.php

--- slm_install.php
+++ slm_install.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title>Instalacja SLM</title>
+<title>SLM Installation</title>
 <META http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
@@ -17,42 +17,42 @@ fputs($f, '$type="admin";'."\n");
 fputs($f, '?>');
 flock($f, LOCK_UN);
 fclose($f);
-echo("Instalacja zakończona pomyślnie!");
+echo("Installation completed successfully!");
 $step="none";
 	} else {
-	echo("Hasła się nie zgadzają!");
+	echo("Passwords do not match!");
 	$step="setadmin";
 	}
 } else if($_POST['step'] == "setadmin" OR $step == "setadmin") {
 mkdir("slm_users");
 mkdir("slm_bans");
 ?>
-<h1>Tworzenie konta administratora</h1><br />
-Skrypt utworzy teraz konto administratora.<br /><br />
+<h1>Creating administrator account</h1><br />
+A script will now create an account for administrator.<br /><br />
 <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
-Nazwa użytkownika: <input type="text" name="adminname" value="admin" /><br />
-Hasło: <input type="password" name="adminpass" /><br />
-Powtórz hasło: <input type="password" name="adminpchk" /><br />
+Username: <input type="text" name="adminname" value="admin" /><br />
+Password: <input type="password" name="adminpass" /><br />
+Repeat password: <input type="password" name="adminpchk" /><br />
 <input type="hidden" name="step" value="end" />
-<input type="submit" value="Stwórz i zakończ" />
+<input type="submit" value="Create and end" />
 </form>
 <?php
 } else if($_POST['step'] == "setfolders") {
 ?>
-<h1>Tworzenie folderów danych</h1><br />
-Skrypt utworzy teraz foldery dla danych SLM - &quot;slm_users&quot; i &quot;slm_bans&quot;. Kliknij przycisk &quot;Dalej&quot;.<br />
+<h1>Creating data directories</h1><br />
+A script will now create directories for SLM data - &quot;slm_users&quot; and &quot;slm_bans&quot;. Click on &quot;Continue&quot;.<br />
 <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 <input type="hidden" name="step" value="setadmin" />
-<input type="submit" value="Dalej" />
+<input type="submit" value="Continue" />
 </form>
 <?php
 } else {
 ?>
-<h1>Instalacja SLM</h1><br />
-Witaj w skrypcie instalacyjnym Phitherek_' s SLM. Zanim rozpoczniesz, upewnij się, że folder, w którym umieściłeś SLM, ma uprawnienia do zapisu dla wszystkich, najlepiej 777 (rwxrwxrwx). Jeżeli jesteś tego pewien, kliknij przycisk &quot;Dalej&quot;.<br />
+<h1>SLM Installation</h1><br />
+Welcome to Phitherek_' s SLM Installation Script. Before you start, make sure that directory of SLM have privileges for *all* to write (the best are 777 (rwxrwxrwx)). If you are sure, click on &quot;Continue&quot;.<br />
 <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 <input type="hidden" name="step" value="setfolders" />
-<input type="submit" value="Dalej" />
+<input type="submit" value="Continue" />
 </form>
 <?php
 }
